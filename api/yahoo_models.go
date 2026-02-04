3bbf69b777ad36dae840aaeb0ee399fed3cdcf09
Brandon Harris <brandon@relentnet.com>
2026-02-03 23:38:44 -0600

    refine(api): use pointers for optional Yahoo fields
    - Mirror Rust Option<T> logic using pointers
    - Use omitempty to clean up JSON for games without leagues
    - Ensure accurate handling of empty XML tags

--- api/yahoo_models.go
+++ api/yahoo_models.go
@@ -21,11 +21,11 @@ type Games struct {
 }
 
 type YahooGame struct {
-	GameKey string  `xml:"game_key" json:"game_key"`
-	GameID  string  `xml:"game_id" json:"game_id"`
-	Name    string  `xml:"name" json:"name"`
-	Code    string  `xml:"code" json:"code"`
-	Leagues Leagues `xml:"leagues" json:"leagues"`
+	GameKey string   `xml:"game_key" json:"game_key"`
+	GameID  string   `xml:"game_id" json:"game_id"`
+	Name    string   `xml:"name" json:"name"`
+	Code    string   `xml:"code" json:"code"`
+	Leagues *Leagues `xml:"leagues,omitempty" json:"leagues,omitempty"`
 }
 
 type Leagues struct {
@@ -33,18 +33,18 @@ type Leagues struct {
 }
 
 type YahooLeague struct {
-	LeagueKey      string `xml:"league_key" json:"league_key"`
-	LeagueID       uint32 `xml:"league_id" json:"league_id"`
-	Name           string `xml:"name" json:"name"`
-	URL            string `xml:"url" json:"url"`
-	LogoURL        string `xml:"logo_url" json:"logo_url"`
-	DraftStatus    string `xml:"draft_status" json:"draft_status"`
-	NumTeams       uint8  `xml:"num_teams" json:"num_teams"`
-	ScoringType    string `xml:"scoring_type" json:"scoring_type"`
-	LeagueType     string `xml:"league_type" json:"league_type"`
-	CurrentWeek    uint8  `xml:"current_week" json:"current_week"`
-	StartWeek      uint8  `xml:"start_week" json:"start_week"`
-	EndWeek        uint8  `xml:"end_week" json:"end_week"`
-	Season         uint16 `xml:"season" json:"season"`
-	GameCode       string `xml:"game_code" json:"game_code"`
+	LeagueKey      string  `xml:"league_key" json:"league_key"`
+	LeagueID       uint32  `xml:"league_id" json:"league_id"`
+	Name           string  `xml:"name" json:"name"`
+	URL            string  `xml:"url" json:"url"`
+	LogoURL        string  `xml:"logo_url" json:"logo_url"`
+	DraftStatus    string  `xml:"draft_status" json:"draft_status"`
+	NumTeams       uint8   `xml:"num_teams" json:"num_teams"`
+	ScoringType    string  `xml:"scoring_type" json:"scoring_type"`
+	LeagueType     string  `xml:"league_type" json:"league_type"`
+	CurrentWeek    *uint8  `xml:"current_week" json:"current_week,omitempty"`
+	StartWeek      *uint8  `xml:"start_week" json:"start_week,omitempty"`
+	EndWeek        *uint8  `xml:"end_week" json:"end_week,omitempty"`
+	Season         uint16  `xml:"season" json:"season"`
+	GameCode       string  `xml:"game_code" json:"game_code"`
 }
