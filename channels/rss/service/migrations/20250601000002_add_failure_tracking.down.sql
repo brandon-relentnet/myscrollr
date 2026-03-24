b2cea638822177334789f29e8db38add7341b9d4
enanimate <spa0703@gmail.com>
2026-03-24 18:05:52 -0400

    fix: correct down migrations for RSS and Sports services
    - RSS migration 002: remove orphaned DROP INDEX (index belongs to 001)
    - Sports migration 002: remove DROP COLUMN for sport/sport_api (part of 001)
    - Mark database migrations strategy complete in v1 checklist

--- channels/rss/service/migrations/20250601000002_add_failure_tracking.down.sql
+++ channels/rss/service/migrations/20250601000002_add_failure_tracking.down.sql
@@ -1,4 +1,3 @@
-DROP INDEX IF EXISTS idx_rss_items_published_at;
 ALTER TABLE tracked_feeds DROP COLUMN IF EXISTS added_by;
 ALTER TABLE tracked_feeds DROP COLUMN IF EXISTS last_success_at;
 ALTER TABLE tracked_feeds DROP COLUMN IF EXISTS last_error_at;
--- channels/sports/service/migrations/20250601000002_add_columns.down.sql
+++ channels/sports/service/migrations/20250601000002_add_columns.down.sql
@@ -6,11 +6,9 @@ ALTER TABLE tracked_leagues DROP COLUMN IF EXISTS country;
 ALTER TABLE tracked_leagues DROP COLUMN IF EXISTS category;
 ALTER TABLE tracked_leagues DROP COLUMN IF EXISTS league_id;
 ALTER TABLE tracked_leagues DROP COLUMN IF EXISTS api_host;
-ALTER TABLE tracked_leagues DROP COLUMN IF EXISTS sport_api;
 
 ALTER TABLE games DROP COLUMN IF EXISTS season;
 ALTER TABLE games DROP COLUMN IF EXISTS venue;
 ALTER TABLE games DROP COLUMN IF EXISTS timer;
 ALTER TABLE games DROP COLUMN IF EXISTS status_long;
 ALTER TABLE games DROP COLUMN IF EXISTS status_short;
-ALTER TABLE games DROP COLUMN IF EXISTS sport;
